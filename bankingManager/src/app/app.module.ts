0c6ff148698efb0500c0235dacbebf94a7f7ff76
Nelson Neto <nelson.ash@outlook.com>
2024-05-26 15:43:28 -0300

    Add details conta component

--- bankingManager/src/app/app.module.ts
+++ bankingManager/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AddContaComponent } from './components/conta/add-conta/add-conta.component';
+import { DetailsContaComponent } from './components/conta/details-conta/details-conta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AddContaComponent,
+    DetailsContaComponent
   ],
   imports: [
     BrowserModule,
---
+++ bankingManager/src/app/components/conta/details-conta/details-conta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-conta',
+  templateUrl: './details-conta.component.html',
+  styleUrl: './details-conta.component.css'
+})
+export class DetailsContaComponent {
+
+}
