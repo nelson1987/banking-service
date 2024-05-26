10f7eda7711ba24b67d23fedc72ce5ae6e4bbb74
Nelson Neto <nelson.ash@outlook.com>
2024-05-26 15:59:28 -0300

    Add routes

--- bankingManager/src/app/app-routing.module.ts
+++ bankingManager/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsContaComponent } from './components/conta/details-conta/details-conta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'},
+  { path: 'Home', component: HomeComponent },
+  { path: 'Conta/:contaId', component: DetailsContaComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
